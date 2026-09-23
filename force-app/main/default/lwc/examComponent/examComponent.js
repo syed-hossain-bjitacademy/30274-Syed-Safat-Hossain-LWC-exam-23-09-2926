6199194090c5aab246a43a1a4c1487d87059d7fb
Vector_BJIT_Academy <vectorbjitacademy@gmail.com>
2026-09-23 16:27:56 +0600

    second commit

--- force-app/main/default/lwc/examComponent/examComponent.js
+++ force-app/main/default/lwc/examComponent/examComponent.js
@@ -1,5 +1,8 @@
 import { LightningElement } from 'lwc';
+import  display  from '@salesforce/apex/MyClass.display';
 
 export default class ExamComponent extends LightningElement {
     country = 'Australia';
+    display = display();
+
 }
